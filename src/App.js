09dedd68e6e649675d41c0f33d78eac8548541f8
soknanprogramming <poch.soknan@gmail.com>
2025-04-13 21:48:03 +0700

    Styling and CSS Basics 1

--- src/App.js
+++ src/App.js
@@ -10,14 +10,15 @@ import React, { Component } from 'react';
 // import styles from './App.module.css';
 // import ParentComponent from './components/ParentComponent';
 // import UserGreeting from './components/UserGreeting';
-import NumberList from './components/NumberList'; 
+// import NumberList from './components/NumberList'; 
+import Stylesheet from './components/Stylesheet';
 
-class App extends Component{
+class App extends Component{ 
   render(){
     // console.log("API KEY:", process.env.REACT_APP_API_KEY);
     return (
-      <div /*className={styles.App}*/>
-        <NumberList />
+      <div>
+        <Stylesheet />
       </div>
     )
   }
