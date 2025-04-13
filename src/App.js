3971b27dbd53af7a72d8ea951fbab2b81a0aaa57
soknanprogramming <poch.soknan@gmail.com>
2025-04-13 22:03:26 +0700

    Inline styling

--- src/App.js
+++ src/App.js
@@ -11,14 +11,15 @@ import React, { Component } from 'react';
 // import ParentComponent from './components/ParentComponent';
 // import UserGreeting from './components/UserGreeting';
 // import NumberList from './components/NumberList'; 
-import Stylesheet from './components/Stylesheet';
+// import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
 
 class App extends Component{ 
   render(){
     // console.log("API KEY:", process.env.REACT_APP_API_KEY);
     return (
       <div>
-        <Stylesheet primary={true} />
+        <Inline />
       </div>
     )
   }
